613557722aea87306886c54ab7fc0ca09bad2d05
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-09-21 17:17:53 +0300

    brand and model entity

--- src/database/entities/brand.entity.ts
+++ src/database/entities/brand.entity.ts
@@ -1,8 +1,12 @@
-import { Column, Entity } from 'typeorm';
+import { Entity, OneToMany, PrimaryColumn } from 'typeorm';
 import { TableNameEnum } from './enums/table-name.enum';
+import { ModelEntity } from './model.entity';
 
 @Entity(TableNameEnum.BRANDS)
 export class BrandEntity {
-  @Column('text')
+  @PrimaryColumn('text', { unique: true })
   title: string;
+
+  @OneToMany(() => ModelEntity, (entity) => entity.brand)
+  models?: ModelEntity[];
 }
--- src/database/entities/model.entity.ts
+++ src/database/entities/model.entity.ts
@@ -1,11 +1,26 @@
-import { Column, Entity, OneToMany } from 'typeorm';
+import {
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+  Unique,
+} from 'typeorm';
 import { AdvertEntity } from './advert.entity';
+import { BrandEntity } from './brand.entity';
 import { TableNameEnum } from './enums/table-name.enum';
 
 @Entity(TableNameEnum.MODELS)
+@Unique('unique_model', ['title', 'brand_id'])
 export class ModelEntity {
-  @Column('text')
+  @PrimaryColumn('text', { name: 'title' })
   title: string;
+
+  @PrimaryColumn('text', { name: 'brand_id' })
+  brand_id: string;
+  @ManyToOne(() => BrandEntity, (entity) => entity.models)
+  @JoinColumn({ name: 'brand_id' })
+  brand: BrandEntity;
 
   @OneToMany(() => AdvertEntity, (entity) => entity.model)
   articles?: AdvertEntity[];
